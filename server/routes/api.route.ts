8b59be1c9e15da06ebed8ae02fc9773fa6a08058
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2026-03-15 12:14:53 +0530

    Added health route

--- server/routes/api.route.ts
+++ server/routes/api.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.route";
+import healthRoutes from "./health.route"
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/health", healthRoutes)
 
 export default router;
